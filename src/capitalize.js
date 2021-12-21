f783d4a69d0887b2e81b14de6c8a1adc5cae6fe3
Vladislav Kolomoiets <vellenger17@gmail.com>
2021-12-21 19:59:55 +0200

    added function to capitalize.js file

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,4 +1,9 @@
 function capitalize(string) {
+	let words = str.split(' ');
+	const newArray = words.map((item) => {
+		return `${item[0].toUpperCase()}${item.slice(1)}`;
+	})
+	return newArray.join(' ');
 }
 
 module.exports = capitalize;
